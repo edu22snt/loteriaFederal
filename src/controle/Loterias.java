136adef810629fc2298036fba434832d4bc6c096
Eduardo Monteiro de Andrade <edu22snt@gmail.com>
2022-12-21 20:11:38 -0300

    Update Loterias.java

--- src/controle/Loterias.java
+++ src/controle/Loterias.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+import enums.LabelsEnum;
+
 /**
  * @author eduardo.andrade
  * @since 01/09/2014
@@ -15,8 +17,10 @@ public class Loterias {
 
 	private String numerosSorteados;
 
-	private String mensagemBancaria = "Ajude o desenvolvedor do sistema" + "\n" +
-			"PIX: CPF: 72446358187 ou emails: edu22snt@gmail.com ou Telefone: (61)98458-8881" + "\n" + "Eduardo Monteiro de Andrade";
+	private String mensagemBancaria =
+			LabelsEnum.CABECALHO.getDescricao() + "\n" +
+			LabelsEnum.DADOSPIX.getDescricao() + "\n" +
+			LabelsEnum.NOMEDESENVOLVEDOR.getDescricao();
 
 	/**
 	 * Metodo responsavel por popular a lista de numeros 
@@ -24,71 +28,10 @@ public class Loterias {
 	 */
 	private void popularListaDeNumerosMaisSorteadosMegaSena() {
 		iniciarLista();
-
 		Integer numerosMaisSorteadosMegaSena[] = listaMegaSena();
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[0]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[1]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[2]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[3]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[4]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[5]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[6]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[7]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[8]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[9]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[10]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[11]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[12]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[13]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[14]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[15]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[16]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[17]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[18]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[19]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[20]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[21]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[22]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[23]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[24]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[25]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[26]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[27]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[28]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[29]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[30]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[31]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[32]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[33]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[34]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[35]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[36]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[37]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[38]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[39]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[40]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[41]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[42]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[43]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[44]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[45]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[46]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[47]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[48]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[49]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[50]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[51]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[52]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[53]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[54]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[55]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[56]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[57]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[58]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[59]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[60]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[61]);
-		listaDeNumeros.add(numerosMaisSorteadosMegaSena[62]);
+		 for(int i = 0; i < 62; i++) {
+			 listaDeNumeros.add(numerosMaisSorteadosMegaSena[i]);
+		 }
 	}
 
 	/**
@@ -97,34 +40,10 @@ public class Loterias {
 	 */
 	private void popularListaDeNumerosMaisSorteadosLotofacil() {
 		iniciarLista();
-
 		Integer numerosMaisSorteadosLotoFacil[] = listaLotoFacil();
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[0]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[1]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[2]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[3]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[4]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[5]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[6]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[7]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[8]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[9]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[10]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[11]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[12]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[13]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[14]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[15]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[16]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[17]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[18]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[19]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[20]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[21]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[22]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[23]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[24]);
-		listaDeNumeros.add(numerosMaisSorteadosLotoFacil[25]);
+		 for(int i = 0; i < 25; i++) {
+			 listaDeNumeros.add(numerosMaisSorteadosLotoFacil[i]);
+		 }
 	}
 
 	private Integer numerosSorteados(Integer numero) {
@@ -139,69 +58,9 @@ public class Loterias {
 		iniciarLista();
 		Integer numerosMaisSorteadosQuina[] = listaQuina();
 
-		/*
-		 * for(int i = 0; i < 59; i++) {
-		 * listaDeNumeros.add(numerosMaisSorteadosQuina[i]); }
-		 */
-		listaDeNumeros.add(numerosMaisSorteadosQuina[1]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[2]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[3]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[4]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[5]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[6]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[7]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[8]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[9]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[10]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[11]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[12]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[13]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[14]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[15]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[16]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[17]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[18]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[19]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[20]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[21]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[22]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[23]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[24]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[25]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[26]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[27]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[28]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[29]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[30]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[31]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[32]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[33]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[34]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[35]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[36]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[37]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[38]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[39]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[40]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[41]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[42]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[43]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[44]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[45]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[46]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[47]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[48]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[49]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[50]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[51]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[52]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[53]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[54]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[55]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[56]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[57]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[58]);
-		listaDeNumeros.add(numerosMaisSorteadosQuina[59]);
+		 for(int i = 0; i < 59; i++) {
+			 listaDeNumeros.add(numerosMaisSorteadosQuina[i]);
+		 }
 	}
 
 	public void sortearNumerosMegaSena() {
